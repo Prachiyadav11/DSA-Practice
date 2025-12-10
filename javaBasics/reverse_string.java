cdddb7bdb0c081a468d2ed6b59bddc5cc7671630
Prachiyadav11 <prachiyadav11102002@gmail.com>
2025-12-10 06:50:45 +0530

    Added reverse String program

---
+++ javaBasics/reverse_string.java
@@ -0,0 +1,15 @@
+public class reverse_string{
+
+    public static void reverseString(String str){
+        String revString = " ";
+        for(int i = str.length()-1; i>=0;i--){
+            revString += str.charAt(i);
+        }
+        System.out.print(revString);
+    }
+
+    public static void main(String args[]){
+        String str = "Apple";
+        reverseString(str);
+    }
+}
