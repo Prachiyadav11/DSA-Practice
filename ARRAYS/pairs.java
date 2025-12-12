3e11cc29da9462ca63589867d9b1a2192d457024
Prachiyadav11 <prachiyadav11102002@gmail.com>
2025-12-12 18:52:15 +0530

    Added the program to print pairs in array

---
+++ ARRAYS/pairs.java
@@ -0,0 +1,21 @@
+public class pairs{
+
+    public static void finadPairs(int arr[]){
+        for(int i = 0; i<arr.length-1; i++){
+            int curr = arr[i];
+            for(int j = i+1;j<arr.length-1;j++){
+                System.out.print("(" + curr+","+arr[j]+")");
+            }
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String args[]){
+        int arr[] = {2,4,6,8,12};
+        finadPairs(arr);
+
+    }
+
+    
+}
